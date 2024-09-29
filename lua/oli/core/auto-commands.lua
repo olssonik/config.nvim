5d9a5ddad3ebd28841e47cb99456dcb00ee32b96
olssonik <borozytoja@gmail.com>
2024-09-29 14:31:23 +0100

    highlight references

--- lua/oli/core/auto-commands.lua
+++ lua/oli/core/auto-commands.lua
@@ -1,7 +1,32 @@
-vim.api.nvim_create_autocmd("TextYankPost", {
-	desc = "Highlight when yanking (copying) text",
-	group = vim.api.nvim_create_augroup("kickstart-highlight-yank", { clear = true }),
-	callback = function()
-		vim.highlight.on_yank()
-	end,
+vim.api.nvim_create_autocmd('TextYankPost', {
+  desc = 'Highlight when yanking (copying) text',
+  group = vim.api.nvim_create_augroup('kickstart-highlight-yank', { clear = true }),
+  callback = function()
+    vim.highlight.on_yank()
+  end,
+})
+vim.api.nvim_create_autocmd('LspAttach', {
+  group = vim.api.nvim_create_augroup('lsp_document_highlight', { clear = true }),
+  callback = function(event)
+    local client = vim.lsp.get_client_by_id(event.data.client_id)
+
+    -- Only attach these features if the server supports documentHighlightProvider
+    if client.server_capabilities.documentHighlightProvider then
+      local highlight_augroup = vim.api.nvim_create_augroup('DocumentHighlight', { clear = true })
+
+      -- Trigger highlights on CursorHold and CursorHoldI
+      vim.api.nvim_create_autocmd({ 'CursorHold', 'CursorHoldI' }, {
+        group = highlight_augroup,
+        buffer = event.buf,
+        callback = vim.lsp.buf.document_highlight,
+      })
+
+      -- Clear the highlights when the cursor moves
+      vim.api.nvim_create_autocmd({ 'CursorMoved', 'CursorMovedI' }, {
+        group = highlight_augroup,
+        buffer = event.buf,
+        callback = vim.lsp.buf.clear_references,
+      })
+    end
+  end,
 })
--- lua/oli/core/options.lua
+++ lua/oli/core/options.lua
@@ -2,6 +2,8 @@ vim.g.netrw_liststyle = 3
 
 local opt = vim.opt
 
+opt.updatetime = 300
+
 opt.relativenumber = true
 opt.number = true
 
@@ -24,14 +26,14 @@ opt.cursorline = false
 -- turn on termguicolors for tokyonight colorscheme to work
 -- (have to use iterm2 or any other true color terminal)
 opt.termguicolors = true
-opt.background = "dark" -- colorschemes that can be light or dark will be made dark
-opt.signcolumn = "yes" -- show sign column so that text doesn't shift
+opt.background = 'dark' -- colorschemes that can be light or dark will be made dark
+opt.signcolumn = 'yes' -- show sign column so that text doesn't shift
 
 -- backspace
-opt.backspace = "indent,eol,start" -- allow backspace on indent, end of line or insert mode start position
+opt.backspace = 'indent,eol,start' -- allow backspace on indent, end of line or insert mode start position
 
 -- clipboard
-opt.clipboard:append("unnamedplus") -- use system clipboard as default register
+opt.clipboard:append 'unnamedplus' -- use system clipboard as default register
 
 -- split windows
 opt.splitright = true -- split vertical window to the right
