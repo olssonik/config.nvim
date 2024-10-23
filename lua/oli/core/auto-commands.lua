70c0e5f55c8a12170d04cdeea782871f584e1e8b
olssonik <borozytoja@gmail.com>
2024-10-23 17:24:12 +0100

    remove highlighting of vars under cursor

--- lua/oli/core/auto-commands.lua
+++ lua/oli/core/auto-commands.lua
@@ -5,31 +5,6 @@ vim.api.nvim_create_autocmd('TextYankPost', {
     vim.highlight.on_yank()
   end,
 })
-vim.api.nvim_create_autocmd('LspAttach', {
-  group = vim.api.nvim_create_augroup('lsp_document_highlight', { clear = true }),
-  callback = function(event)
-    local client = vim.lsp.get_client_by_id(event.data.client_id)
-
-    -- Only attach these features if the server supports documentHighlightProvider
-    if client.server_capabilities.documentHighlightProvider then
-      local highlight_augroup = vim.api.nvim_create_augroup('DocumentHighlight', { clear = true })
-
-      -- Trigger highlights on CursorHold and CursorHoldI
-      vim.api.nvim_create_autocmd({ 'CursorHold', 'CursorHoldI' }, {
-        group = highlight_augroup,
-        buffer = event.buf,
-        callback = vim.lsp.buf.document_highlight,
-      })
-
-      -- Clear the highlights when the cursor moves
-      vim.api.nvim_create_autocmd({ 'CursorMoved', 'CursorMovedI' }, {
-        group = highlight_augroup,
-        buffer = event.buf,
-        callback = vim.lsp.buf.clear_references,
-      })
-    end
-  end,
-})
 -- reload file if changed outside but buffer open
 vim.o.autoread = true
 vim.api.nvim_create_autocmd({ "BufEnter", "CursorHold", "CursorHoldI", "FocusGained" }, {
